8b41d235a7c31ba24d2cb134b1ae999a94cc6668
mertdorukkalkan <mertdorukkalkan@gmail.com>
2024-05-02 17:50:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press ⌃F5 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import math
+
+
+points = [(1, 2), (3, 5), (6, 1), (7, 7), (4, 4)]
+def euclideanDistance(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+
+min_distance = min(distances)
+print(f"En küçük Öklid mesafesi: {min_distance}")
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
